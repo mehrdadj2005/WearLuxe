0de03187c62ff2fb1d90b9dd5c4d0c253eb2b016
farhad-ahmadi-04 <farhad.13ahmadi.83@gmail.com>
2025-04-14 19:17:58 +0430

    refactor: add nav list for navigation

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,24 +1,34 @@
-import { Button, Typography } from "@mui/material";
+import { Button, Container, Typography } from "@mui/material";
 import Searchbox from "./ui/searchbox";
-import { ForkLeft, KeyboardArrowLeft, Person2Outlined } from "@mui/icons-material";
+import { KeyboardArrowLeft, Person2Outlined } from "@mui/icons-material";
+import { categories } from "@/config/category";
+import Link from "next/link";
+import Nav from "./ui/nav";
 
 function Header() {
     return (
-        <header className="flex justify-between items-center">
-            <div>
-                <Typography variant="h2">Logo</Typography>
+        <>
+            <header>
+                <Container className="flex justify-between items-center">
+                    <div>
+                        <Typography variant="h2">Logo</Typography>
+                    </div>
+                    <div className="w-4/12">
+                        <Searchbox />
+                    </div>
+                    <Button
+                        variant="outlined"
+                        startIcon={<Person2Outlined />}
+                        endIcon={<KeyboardArrowLeft />}
+                        size="medium"
+                        className="flex gap-2"
+                    >وارد شوید</Button>
+                </Container>
+            </header>
+            <div className="bg-primary-50">
+                <Nav />
             </div>
-            <div className="w-4/12">
-                <Searchbox />
-            </div>
-            <Button
-                variant="outlined"
-                startIcon={<Person2Outlined />}
-                endIcon={<KeyboardArrowLeft />}
-                size="medium"
-                className="flex gap-2"
-            >وارد شوید</Button>
-        </header>
+        </>
     );
 }
 
