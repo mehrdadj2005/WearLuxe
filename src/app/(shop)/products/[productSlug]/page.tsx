62b5118c5fc6296fc6c8b0c1e3f96c56c4943f42
farhad-ahmadi-04 <farhad.13ahmadi.83@gmail.com>
2025-05-06 02:12:22 +0430

    feat: enhance ProductId component with detailed product information display

--- src/app/(shop)/products/[productSlug]/page.tsx
+++ src/app/(shop)/products/[productSlug]/page.tsx
@@ -4,6 +4,7 @@ import { getProduct } from "@/services/getProduct";
 import { Box, Divider, Grid, List, ListItem, Typography } from "@mui/material";
 import Image from "next/image";
 import ProductError from "./error";
+import { ArrowLeft, ForkLeftOutlined } from "@mui/icons-material";
 
 interface ICategory {
     params: Promise<{ productSlug: string }>;
@@ -22,6 +23,7 @@ async function ProductId({ params }: ICategory) {
     if (error) return <ProductError error={error} />
     if (!data) return <ProductError error={error} />
 
+    console.log(data);
 
     return (
         <Container>
@@ -37,25 +39,59 @@ async function ProductId({ params }: ICategory) {
                     />
                 </Grid>
                 <Grid size={{ xs: 12, md: 8 }}>
-                    <Box>
-                        <Typography variant="h3">{data?.name}</Typography>
-                        <Box >
-                            <Typography sx={{ backgroundColor: "var(--color-primary-200)", width: "fit-content", paddingX: 2, paddingY: .5, borderRadius: 50, marginTop: 2 }} variant="h6">{data?.price.toLocaleString()} تومان</Typography>
+                    <Box sx={{ display: "flex", flexDirection: { md: "column" }, justifyContent: "space-between" }}>
+                        <Typography variant="h5">{data?.name}</Typography>
+                        <Box sx={{ display: "flex", gap: 1, alignItems: { xs: "start", md: "center" }, marginTop: { xs: 0, md: 1 } }}>
+                            <Typography sx={{ display: { xs: "none", md: "block" } }} color="primary" variant="h6">{data.discountedPercentage}%</Typography>
+                            <Box sx={{ display: "flex", flexDirection: { xs: "column", sm: "row" }, alignItems: "center", gap: { xs: 1, md: 0 } }}>
+                                <Typography sx={{ backgroundColor: "var(--color-primary-200)", width: "fit-content", paddingX: 2, paddingY: .5, borderRadius: 1, textDecorationLine: data.discountedPercentage ? "line-through" : "none" }}>
+                                    {data?.price.toLocaleString()} تومان
+                                </Typography>
+                                {data.discountedPercentage &&
+                                    <>
+                                        <ArrowLeft sx={{ display: { xs: "none", md: "block" } }} />
+                                        <Typography sx={{ width: "fit-content", paddingX: 2, paddingY: .5, borderRadius: 1, backgroundColor: "var(--color-primary-400)" }}>
+                                            {data?.discountedPrice.toLocaleString()} تومان
+                                        </Typography>
+                                    </>
+                                }
+                            </Box>
                         </Box>
                     </Box>
-                    <List>
-                        <ListItem sx={{ paddingLeft: 0, paddingRight: 0 }}>سایز: {data?.sizes.join(", ")}</ListItem>
-                        <ListItem sx={{ paddingLeft: 0, paddingRight: 0 }}>رنگ: {data?.colors.join(", ")}</ListItem>
-                        <ListItem sx={{ paddingLeft: 0, paddingRight: 0 }}>موجودی: {data?.stock ? "موجود" : "ناموجود"}</ListItem>
-                        <ListItem sx={{ paddingLeft: 0, paddingRight: 0 }}>توضیحات: {data?.description}</ListItem>
+
+                    <List sx={{ display: "flex", flexDirection: { xs: "row", md: "column" }, paddingY: 2 }}>
+                        <ListItem sx={{ paddingX: { xs: 1, md: 0 }, display: "flex", flexDirection: { xs: "column", md: "row" }, alignItems: { xs: "center", md: "flex-start" }, width: { xs: "40%", md: "100%" }, borderLeft: { xs: "1.5px solid rgba(0, 0, 0, 0.12)", md: 0 } }} >
+                            <Typography variant="body2">سایز</Typography>
+                            <Typography color="textDisabled" sx={{ display: { xs: "none", md: "block" }, marginLeft: 1 }}>:</Typography>
+                            <Typography>{data?.sizes.join(", ")}</Typography>
+                        </ListItem>
+
+                        <ListItem sx={{ paddingLeft: 0, paddingRight: 0, display: "flex", flexDirection: { xs: "column", md: "row" }, alignItems: { xs: "center", md: "flex-start" }, width: "100%" }}>
+                            <Typography variant="body2">رنگ</Typography>
+                            <Typography color="textDisabled" sx={{ display: { xs: "none", md: "block" }, marginLeft: 1 }}>:</Typography>
+                            <Typography>{data?.colors.join(", ")}</Typography>
+                        </ListItem>
+
+                        <ListItem sx={{ paddingX: { xs: 1, md: 0 }, display: "flex", flexDirection: { xs: "column", md: "row" }, alignItems: { xs: "center", md: "flex-start" }, width: { xs: "40%", md: "100%" }, borderRight: { xs: "1.5px solid rgba(0, 0, 0, 0.12)", md: 0 } }} >
+                            <Typography variant="body2">موجودی</Typography>
+                            <Typography color="textDisabled" sx={{ display: { xs: "none", md: "block" }, marginLeft: 1 }}>:</Typography>
+                            <Typography>{data?.stock ? "موجود" : "ناموجود"}</Typography>
+                        </ListItem>
+
+                        <ListItem sx={{ paddingLeft: 0, paddingRight: 0, display: { xs: "none", md: "flex" }, flexDirection: { xs: "column", md: "row" }, alignItems: { xs: "center", md: "flex-start" } }}>
+                            <Typography variant="body2">توضیحات</Typography>
+                            <Typography color="textDisabled" sx={{ marginLeft: 1 }}>:</Typography>
+                            <Typography>{data?.description}</Typography>
+                        </ListItem>
                     </List>
+
                     <Divider />
                     <Box sx={{ marginTop: 1 }}>
                         <SizeToggleButton />
                     </Box>
                 </Grid>
-            </Grid>
-        </Container>
+            </Grid >
+        </Container >
     );
 }
 
