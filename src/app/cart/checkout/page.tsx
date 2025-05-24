5cb75df6fce7a65a4e2789acd918910f34a241a7
farhad-ahmadi-04 <farhad.13ahmadi.83@gmail.com>
2025-05-24 22:12:04 +0430

    feat: add Checkout component for the checkout page

---
+++ src/app/cart/checkout/page.tsx
@@ -0,0 +1,10 @@
+function Checkout() {
+    return (
+        <div>
+            <h1>Checkout Page</h1>
+            {/* Additional checkout components will go here */}
+        </div>
+    );
+}
+
+export default Checkout;
