713d2c9a8821f62e40bee209898f46933e81ab3f
farhad-ahmadi-04 <farhad.13ahmadi.83@gmail.com>
2025-05-24 22:11:20 +0430

    feat: add OrderSummary component for displaying cart summary and total price

---
+++ src/app/cart/orderSummary.tsx
@@ -0,0 +1,45 @@
+"use client"
+
+import { Box, Button, Divider, Paper, Typography } from "@mui/material";
+import Link from "next/link";
+import { getTotalCartPrice } from "./cartSlice";
+import { useSelector } from "react-redux";
+import { formatPrice } from "@/helper/helper";
+
+function OrderSummary() {
+    const totalPrice = useSelector(getTotalCartPrice)
+
+    return (
+        <Paper elevation={2} sx={{ p: 3, position: "sticky", top: 20 }}>
+            <Typography variant="h6" gutterBottom>
+                خلاصه سفارش
+            </Typography>
+            <Divider sx={{ mb: 2 }} />
+
+            <Box sx={{ display: "flex", justifyContent: "space-between", mb: 1 }}>
+                <Typography>جمع سبد خرید</Typography>
+                <Typography>{formatPrice(totalPrice)}</Typography>
+            </Box>
+
+            <Box sx={{ display: "flex", justifyContent: "space-between", mb: 1 }}>
+                <Typography>هزینه ارسال</Typography>
+                <Typography>{formatPrice(150000)}</Typography>
+            </Box>
+
+            <Divider sx={{ my: 2 }} />
+
+            <Box sx={{ display: "flex", justifyContent: "space-between", mb: 3 }}>
+                <Typography variant="h6">مبلغ قابل پرداخت</Typography>
+                <Typography variant="h6">{formatPrice(totalPrice + 150000)}</Typography>
+            </Box>
+
+            <Link href="/cart/checkout">
+                <Button variant="contained" color="primary" fullWidth size="large" sx={{ mt: 2 }}>
+                    ادامه فرآیند خرید
+                </Button>
+            </Link>
+        </Paper>
+    );
+}
+
+export default OrderSummary;
