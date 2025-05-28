3497a4dd04a4ee048280e6999f5194a5cfef4bc6
mehrdadj2005 <mehrdadjokari13@gmail.com>
2025-05-28 22:29:34 +0330

    refactor: consolidate CategoryPage layout and remove unused layout component

--- src/app/(shop)/products/category/[categorySlug]/page.tsx
+++ src/app/(shop)/products/category/[categorySlug]/page.tsx
@@ -1,34 +1,92 @@
+import ProductDetailsFilter from "@/app/(shop)/products/category/[categorySlug]/productDetailsFilter";
+import ProductDetailsSort from "@/app/(shop)/products/category/[categorySlug]/productDetailsSort";
+import Container from "@/components/container";
 import ProductCard from "@/components/products/productCard";
-import { categories } from "@/config/category";
-import { catsCategory } from "@/config/catsCategory";
+import { getProduct } from "@/services/getProduct";
 import { Box } from "@mui/material";
-
-interface ICategory {
-  params: Promise<{ categorySlug: string }>;
+interface ICategoryPageProps {
+  searchParams: Promise<{ sort: string }>;
 }
 
-export async function generateStaticParams() {
-  return categories.map((category) => ({
-    categorySlug: category.slug,
-  }));
-}
+export default async function CategoryPage({
+  searchParams,
+}: ICategoryPageProps) {
+  const data = (await searchParams) as Record<string, string>;
 
-async function CategoryPage({ params }: ICategory) {
-  console.log(await params);
+  let filters = "";
+  for (const i in data) {
+    filters += `${i}=${data[i]}&`;
+  }
 
-  // return (<div>category: {(await params).categorySlug}</div>);
+  const products = await getProduct(
+    `http://localhost:4000/products?${filters}`
+  );
+
   return (
-    <Box className="w-full px-0 flex flex-wrap justify-start">
-      {catsCategory.products.map((item) => (
+    <Container
+      sx={{
+        display: "flex",
+        flexDirection: { xs: "column", xl: "row" },
+        pt: 2,
+      }}
+    >
+      <Box
+        sx={{
+          display: { xs: "none", xl: "flex" },
+        }}
+      >
+        <ProductDetailsFilter showDrawerFilter={false} />
+      </Box>
+      <Box
+        sx={{
+          display: "flex",
+          flexDirection: "column",
+          width: "100%",
+        }}
+      >
         <Box
-          key={item.id}
-          className="p-2 w-1/2 sm:w-1/3 lg:w-1/4 flex justify-center"
+          sx={{
+            display: "flex",
+            gap: 2,
+          }}
         >
-          <ProductCard {...item} />
+          <Box
+            sx={{
+              display: { xs: "flex", xl: "none" },
+            }}
+          >
+            <ProductDetailsFilter showDrawerFilter={false} />
+          </Box>
+          <ProductDetailsSort showDrawerSort={false} />
         </Box>
-      ))}
-    </Box>
+        <Box
+          sx={{
+            width: "100%",
+            px: 0,
+            display: "flex",
+            flexWrap: "wrap",
+            justifyContent: "flex-start",
+          }}
+        >
+          {products.data.map((item) => (
+            <Box
+              key={item.id}
+              sx={{
+                p: 2,
+                width: {
+                  xs: "50%",
+                  sm: "33.3333%",
+                  lg: "25%",
+                },
+                display: "flex",
+                justifyContent: "center",
+              }}
+            >
+              <ProductCard {...item} />
+            </Box>
+          ))}
+        </Box>
+      </Box>
+    </Container>
   );
 }
-
-export default CategoryPage;
