04ef5c701efe5838cdc616adc271c455be635d1c
mehrdadj2005 <mehrdadjokari13@gmail.com>
2025-05-30 00:48:49 +0330

    refactor: update HomePage to fetch product data dynamically and improve component rendering

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,25 +1,37 @@
 import Container from "@/components/container";
 import HeroSection from "@/components/layout/hero";
 import ProductSlider from "@/components/products/productSlider";
-import { catsCategory } from "@/config/catsCategory";
-import { setsCategory } from "@/config/setsCategory";
-import { shirtsCategory } from "@/config/shirtsCategory";
+import { getProduct } from "@/services/getProduct";
+import { IProduct } from "@/types/product";
 import { Box, CardMedia } from "@mui/material";
 
-export default function HomePage() {
+export default async function HomePage() {
+  const dataShirts = await getProduct(
+    "http://localhost:4000/products?categoryId=1"
+  );
+  const dataPants = await getProduct(
+    "http://localhost:4000/products?categoryId=2"
+  );
+  const dataCats = await getProduct(
+    "http://localhost:4000/products?categoryId=3"
+  );
+  const dataSets = await getProduct(
+    "http://localhost:4000/products?categoryId=4"
+  );
+
   return (
     <>
       <HeroSection />
       <Box className="bg-primary-200 py-4 md:py-8">
-        <ProductSlider data={catsCategory} />
+        <ProductSlider data={dataPants.data as IProduct[]} />
       </Box>
-      <ProductSlider data={setsCategory} />
-      <ProductSlider data={shirtsCategory} />
+      <ProductSlider data={dataShirts.data as IProduct[]} />
+      <ProductSlider data={dataCats.data as IProduct[]} />
       <Box className="bg-secondary-200 py-4 md:py-8">
-        <ProductSlider data={catsCategory} />
+        <ProductSlider data={dataSets.data as IProduct[]} />
       </Box>
-      <ProductSlider data={setsCategory} />
-      <ProductSlider data={shirtsCategory} />
+      <ProductSlider data={dataPants.data as IProduct[]} />
+      <ProductSlider data={dataShirts.data as IProduct[]} />
       <Container>
         <Box className="grid grid-cols-2 lg:grid-cols-4 gap-3 lg:gap-4 px-8 md:px-16 py-4 md:py-8">
           <CardMedia
@@ -48,8 +60,8 @@ export default function HomePage() {
           />
         </Box>
       </Container>
-      <ProductSlider data={setsCategory} />
-      <ProductSlider data={shirtsCategory} />
+      <ProductSlider data={dataCats.data as IProduct[]} />
+      <ProductSlider data={dataSets.data as IProduct[]} />
     </>
   );
 }
